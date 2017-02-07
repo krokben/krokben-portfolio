1d03d60a11060fae42f860691fc646966c0f71ad
krokben <tombookpro@Tombooks-MacBook-Pro.local>
2017-02-07 23:42:56 +0100

    Skills section

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var parallax = document.querySelector('.circle'), speed = 0.1;
 
+
+// Scroll functionality
 window.onscroll = function() {
 	var windowYOffset = window.pageYOffset,
 	  elBackgroundPos = (windowYOffset * speed) + 'px';
@@ -7,8 +9,8 @@ window.onscroll = function() {
 	parallax.style.backgroundPosition = elBackgroundPos;
 };
 
-const downBtn = document.getElementById('downBtn');
-const upBtn = document.getElementById('upBtn');
+const downBtn = document.querySelector('#downBtn');
+const upBtn = document.querySelector('#upBtn');
 
 downBtn.addEventListener('click', function() {
 	$('#aboutMe').scrollintoview({
@@ -23,3 +25,47 @@ upBtn.addEventListener('click', function() {
 		direction: 'vertical'
 	});
 });
+
+// Skills
+const skillsArea = document.querySelectorAll('.skills-area');
+const topBars = document.querySelector('#topBars');
+const bottomBars = document.querySelector('#bottomBars');
+
+skillsArea.forEach((item) => item.addEventListener('click', () => {
+	let bar;
+	const area = item.children[0].id;
+
+	if (item.parentElement.className === 'row') {
+		bar = topBars;
+	} else {
+		bar = bottomBars;
+	}
+	showSkills(bar, area);
+}));
+
+function showSkills(bar, area) {
+	let i;
+	const array = Array.from(bar.children);
+
+	switch(area) {
+		case 'html':		i = 10; break;
+		case 'css':			i = 8; 	break;
+		case 'javascript': 	i = 8; 	break;
+		case 'jquery':		i = 8; 	break;
+		case 'php': 		i = 6; 	break;
+		case 'react': 		i = 5; 	break;
+		case 'bootstrap': 	i = 7; 	break;
+		case 'wordpress': 	i = 1; 	break;
+		case 'sass': 		i = 1; 	break;
+		case 'scrum': 		i = 10; break;
+		default: 			i = 10;
+	}
+
+	array.forEach((child, idx) => {
+		if (idx >= i) {
+			child.className = "";
+		} else {
+			child.className = area;
+		}
+	});
+}
