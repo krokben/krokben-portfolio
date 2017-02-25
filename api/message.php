2b4b74223cf7e1b30453c67c1a956b0f5b904847
krokben <tombookpro@Tombooks-MacBook-Pro.local>
2017-02-25 20:43:11 +0100

    new carousel, new scrollto, css fixes

--- api/message.php
+++ api/message.php
@@ -1,12 +1,13 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root','' , 'krokben');
+$db = mysqli_connect('krokben-219508.mysql.binero.se', '219508_aq20643','crookbone' , '219508-krokben');
+// $db = mysqli_connect('localhost', 'root','' , 'krokben');
 
 $name = mysqli_real_escape_string($db, $_POST['name']);
 $email = mysqli_real_escape_string($db, $_POST['email']);
 $message = mysqli_real_escape_string($db, $_POST['message']);
 
 if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
-	mail("svensson.tommy@student.kyh.se", "Mail till krokben.se från " . $name . " " . $email, $message);
+	mail("svensson.tommy@student.kyh.se", "Mail till krokben.se från " . $name, $message . " (Sent from $email)");
 	echo "thank you!";
 }
--- script.js
+++ script.js
@@ -1,7 +1,96 @@
-var parallax = document.querySelector('.circle'), speed = 0.1;
+/* Flickity */
+const portfolioInfo = document.querySelector('#portfolioInfo');
 
+const portfolioText = {
+	init: `
+		<h3>past work</h3>
+        <p>Here are some of the things I've worked on in the past. Click on one to open it in a new tab.</p>
+	`,
+	tile: `
+    	<h3><a href="http://krokben.se" target="_blank">tile game</a></h3>
+        <p>A simple little game I made where you click tiles until they form 3 vertical lines.</p>
+    `,
+    memory: `
+    	<h3><a href="http://krokben.se" target="_blank">memory game</a></h3>
+        <p>A simple memory game made with vanilla js. Focus is on good, intuitive design.</p>
+    `,
+	geten: `
+		<h3><a href="http://krokben.se" target="_blank">den glada geten</a></h3>
+        <p>A team project at KYH. Together we made a website for a fictional bed & breakfast.</p>
+    `,
+    hangman: `
+		<h3><a href="http://krokben.se" target="_blank">hangman</a></h3>
+        <p>A silly litte Hangman game with rudimentary pictures drawn in Paint.</p>
+    `
+}
 
-// Scroll functionality
+const flkty = new Flickity( '.carousel', {
+	// options
+	cellAlign: 'left',
+	contain: true,
+	freeScroll: true,
+	wrapAround: true,
+	pageDots: false
+});
+
+flkty.on('select', function() {
+	switch(flkty.selectedIndex) {
+		case 1:
+			portfolioInfo.innerHTML = portfolioText.tile;
+			break;
+		case 2:
+			portfolioInfo.innerHTML = portfolioText.memory;
+			break;
+		case 3:
+			portfolioInfo.innerHTML = portfolioText.geten;
+			break;
+		case 4:
+			portfolioInfo.innerHTML = portfolioText.hangman;
+			break;
+		default:
+			portfolioInfo.innerHTML = portfolioText.init;
+	}
+});
+
+const Sidenav = (function() {
+	/* Sidenav */
+	const body = document.querySelector('body');
+	const trigram = document.querySelector('#trigram');
+	const trigramExit = document.querySelector('.trigram');
+	const container = document.querySelector('#container');
+	const sidenav = document.querySelector('#sidenav');
+
+	trigram.addEventListener('click', toggleSidenav);
+	trigramExit.addEventListener('click', toggleSidenav);
+
+	function toggleSidenav() {
+		if (sidenav.className !== 'active') {
+			container.className = 'active';
+			sidenav.className = 'active';
+	    	body.style.overflow = 'hidden';
+	    	trigram.className = 'hidden';
+	    	trigramExit.id = 'show';
+		} else {
+			container.className = '';
+			sidenav.className = '';
+	    	body.style.overflow = 'initial';
+	    	trigram.className = '';
+	    	trigramExit.id = '';
+		}
+	}
+
+	return {
+		trigram,
+		trigramExit
+	}
+})();
+
+/* Scroll functionality */
+const parallax = document.querySelector('.circle'), speed = 0.1;
+const body = document.querySelector('body');
+const sidenav = document.querySelector('#sidenav');
+const container = document.querySelector('#container');
+
 window.onscroll = function() {
 	var windowYOffset = window.pageYOffset,
 	  elBackgroundPos = (windowYOffset * speed) + 'px';
@@ -9,22 +98,43 @@ window.onscroll = function() {
 	parallax.style.backgroundPosition = elBackgroundPos;
 };
 
+// Links
 const downBtn = document.querySelector('#downBtn');
-// const upBtn = document.querySelector('#upBtn');
 
-downBtn.addEventListener('click', function() {
-	$('#aboutMe').scrollintoview({
-		duration: 1000,
-		direction: 'vertical'
-	});
-});
+const homeLink = document.querySelectorAll('.home');
+const aboutLink = document.querySelectorAll('.about');
+const skillsLink = document.querySelectorAll('.skills');
+const portfolioLink = document.querySelectorAll('.portfolio');
+const contactLink = document.querySelectorAll('.contact');
 
-// upBtn.addEventListener('click', function() {
-// 	$('body').scrollintoview({
-// 		duration: 1000,
-// 		direction: 'vertical'
-// 	});
-// });
+// Locations
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const skills = document.querySelector('#skills');
+const portfolio = document.querySelector('#portfolio');
+const contact = document.querySelector('#contact');
+
+// Event listeners
+downBtn.addEventListener('click', () => scrollTo(about));
+homeLink.forEach((x) => x.addEventListener('click', () => scrollTo(home)));
+aboutLink.forEach((x) => x.addEventListener('click', () => scrollTo(about)));
+skillsLink.forEach((x) => x.addEventListener('click', () => scrollTo(skills)));
+portfolioLink.forEach((x) => x.addEventListener('click', () => scrollTo(portfolio)));
+contactLink.forEach((x) => x.addEventListener('click', () => scrollTo(contact)));
+
+function scrollTo(location) {
+	if (sidenav.className === 'active') {
+		sidenav.className = 'inactive';
+		container.className = 'inactive'; // turn off transition
+		body.style.overflow = 'initial';
+		Sidenav.trigram.className = '';
+		Sidenav.trigramExit.id = '';
+		window.smoothScroll(location);
+	} else {
+		window.smoothScroll(location, 1000); // 1 sec scroll
+	}
+	container.className = 'pre-active'; // turn on transition
+}
 
 // Skills
 const skillsArea = document.querySelectorAll('.skills-area');
@@ -50,9 +160,9 @@ function showSkills(bar, area) {
 	switch(area) {
 		case 'html':		i = 10; break;
 		case 'css':			i = 8; 	break;
-		case 'javascript': 	i = 8; 	break;
-		case 'jquery':		i = 8; 	break;
-		case 'php': 		i = 6; 	break;
+		case 'javascript': 	i = 7; 	break;
+		case 'jquery':		i = 7; 	break;
+		case 'php': 		i = 4; 	break;
 		case 'react': 		i = 5; 	break;
 		case 'bootstrap': 	i = 7; 	break;
 		case 'wordpress': 	i = 1; 	break;
@@ -70,85 +180,7 @@ function showSkills(bar, area) {
 	});
 }
 
-// Portfolio
-let portfolio1 = document.querySelector('#one');
-let portfolio2 = document.querySelector('#two');
-let portfolio3 = document.querySelector('#three');
-
-cacheAndBind();
-
-function cacheAndBind() {
-	portfolio1 = document.querySelector('#one');
-	portfolio2 = document.querySelector('#two');
-	portfolio3 = document.querySelector('#three');
-	portfolio1.addEventListener('click', rotate);
-	portfolio2.addEventListener('click', rotate);
-	portfolio3.addEventListener('click', rotate);
-}
-
-function rotate() {
-	if (this.className === 'one') {
-		// Rotate left
-		switch(portfolio1.className) {
-			case 'one':
-				portfolio1.classList.add('opacity0');
-				portfolio2.className = 'one';
-				portfolio3.className = 'two';
-				setTimeout(function() {
-					portfolio1.className = 'three';
-				}, 200);
-				break;
-			case 'two':
-				portfolio1.className = 'one';
-				portfolio2.className = 'two';
-				portfolio3.classList.add('opacity0');
-				setTimeout(function() {
-					portfolio3.className = 'three';
-				}, 200);
-				break;
-			case 'three':
-				portfolio1.className = 'two';
-				portfolio2.classList.add('opacity0');
-				portfolio3.className = 'one';
-				setTimeout(function() {
-					portfolio2.className = 'three';
-				}, 200);
-				break;
-		}
-	} else if (this.className === 'three') {
-		// Rotate right
-		switch(portfolio1.className) {
-			case 'one':
-				portfolio1.className = 'two';
-				portfolio2.className = 'three';
-				portfolio3.classList.add('opacity0');
-				setTimeout(function() {
-					portfolio3.className = 'one';
-				}, 200);
-				break;
-			case 'two':
-				portfolio1.className = 'three';
-				portfolio2.classList.add('opacity0');
-				portfolio3.className = 'two';
-				setTimeout(function() {
-					portfolio2.className = 'one';
-				}, 200);
-				break;
-			case 'three':
-				portfolio1.classList.add('opacity0');
-				portfolio2.className = 'two'
-				portfolio3.className = 'three';
-				setTimeout(function() {
-					portfolio1.className = 'one';
-				}, 200);
-				break;
-		}
-	}
-
-	cacheAndBind();
-}
-
-// Contact form
+/* Contact form */
 const contactForm = document.querySelector('#contactForm');
 const submitBtn = document.querySelector('#submitBtn');
 
